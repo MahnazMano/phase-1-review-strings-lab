eba0256211fbdb09b865b877bc84be3b5ba7caae
mahnaz nazir <mahnaz.n.ibrahim@gmail.com>
2022-12-04 19:38:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+// Write your code in this file!
+const currentUser = "Grace Hopper";
+() => {
+  let welcomeMessage = "Welcome to Flatbook, ";
+};
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+let shortGreeting = `Welcome, ${currentUser.slice(0, 11)}!`;
